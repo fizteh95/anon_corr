46f61388b37b69bf749b5514787efdaf7281b24b
fizteh95 <40636393+fizteh95@users.noreply.github.com>
2020-03-11 10:40:51 +0500

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get('BOT_TOKEN') or "1073013544:AAHsyaQ2s-mE2pLd0Y-KjsiobcfCMhZqomw"
     BOT_USER_NAME = os.environ.get('BOT_USER_NAME') or "ClaimantBot"
     # URL = os.environ.get('URL') or "THE-HEROKU-APP-LINK"
-    ADMIN = os.environ.get('ADMIN') or "avshubin"  # "avshubin" Konst_test
+    ADMIN = os.environ.get('ADMIN') or "Konst_test"  # "avshubin" "Konst_test"
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
