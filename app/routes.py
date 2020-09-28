675bb823c3e87af890ffe4fd19bc72df3ddc2998
fizteh95 <40636393+fizteh95@users.noreply.github.com>
2020-09-28 14:40:05 +0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -154,7 +154,8 @@ def make_cmd(cmd=None, name=None):
 
     elif (cmd == 'отправить_всем') and name:
         cs_raw = Link.query.distinct(Link.from_chat_id)
-        for clai in cs_raw:
+        cs = [x.from_chat_id for x in cs_raw]
+        for clai in cs:
             print(clai)
             bot.sendMessage(chat_id=clai, text=name)
         bot.sendMessage(chat_id=admin_cid, text='Сообщение отправлено всем взыскателям.')
